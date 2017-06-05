6b7079c4b4934d2755e2836c58418ea6cdc2329a
NadimRai <rai_nadim@yahoo.com>
2017-06-05 02:53:48 -0700

    created users/show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
 
+  resources :users, only: [:show]
   get 'seller' , to: 'events#currentuser'
   root 'events#index'
 end
