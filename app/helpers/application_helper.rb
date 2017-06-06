ebaf47b015e2591b37be7c8af10b5d4853780c86
NadimRai <rai_nadim@yahoo.com>
2017-06-05 23:19:18 -0700

    Admin added to navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
 	def time_format(event_date)
 		event_date.strftime("%A, %d %b %Y %l:%M %p")
 	end
+
+	def event_status(event)
+    if event.end_date <= Date.today
+      content_tag(:span, "", class: "past")
+    else
+      content_tag(:span, "", class: "upcoming")
+    end
+  end
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
 end
