e366c4e5bab5f91cc982712a9755b7ed35a188e5
NadimRai <rai_nadim@yahoo.com>
2017-06-06 18:19:04 -0700

    ability to display category in view/categories

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
 
+  resources :categories, only: [:show]
+
   resources :users, only: [:show]
   get 'seller' , to: 'events#currentuser'
   root 'events#index'
