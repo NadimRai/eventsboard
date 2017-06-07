6a8235b4af1ad41e0088e78deca095df9a24a460
NadimRai <rai_nadim@yahoo.com>
2017-06-07 03:19:44 -0700

    added friendly_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607100340) do
+ActiveRecord::Schema.define(version: 20170607101412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,8 +70,10 @@ ActiveRecord::Schema.define(version: 20170607100340) do
     t.string "username"
     t.string "organization_name"
     t.boolean "admin", default: false
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "events", "users"
