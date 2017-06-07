08b3667fbfe9bef7be14dd8a1ea2cebfa3c126d9
NadimRai <rai_nadim@yahoo.com>
2017-06-07 00:16:34 -0700

    added image field to Events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606235942) do
+ActiveRecord::Schema.define(version: 20170607071502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170606235942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
