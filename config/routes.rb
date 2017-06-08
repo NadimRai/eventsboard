504e7d68e7d73b80ac843e192ffb606a336e1635
NadimRai <rai_nadim@yahoo.com>
2017-06-07 19:24:28 -0700

    generated Comments controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    resources :comments, only: [:create]
+  end
 
   resources :categories, only: [:show]
 
