4148cf0ed5b6ffa3afcdb99341d0f0ca11d3889c
NadimRai <rai_nadim@yahoo.com>
2017-06-08 01:25:05 -0700

    generated controller Attendances

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
     resources :comments, only: [:create]
+    resources :attendances, only: [:create]
   end
 
   resources :categories, only: [:show]
