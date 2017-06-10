94ac963091db753310e321f6d39a02a5dc35ffdd
NadimRai <rai_nadim@yahoo.com>
2017-06-09 23:24:31 -0700

    generated Likes model

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
+    resources :likes, only: [:create]
     resources :comments, only: [:create]
     resources :attendances, only: [:create]
   end
